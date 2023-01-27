b6be9e16e4566b82af1053919a6658f29836e054
Michele Nucci <mik.nucci@gmail.com>
2023-01-27 01:43:10 +0100

    Data files updated and removed no more needed files

--- XamarinSampleCollectionView/GlobalUsing.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System;
-global using System.ComponentModel;
-global using System.Globalization;
-global using System.Runtime.CompilerServices;
-global using Newtonsoft.Json;
-global using Newtonsoft.Json.Converters;
-global using XamarinSampleCollectionView.Model;
